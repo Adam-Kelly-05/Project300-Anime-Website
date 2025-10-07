2b3d99983dc3ac9ba375fee5eca7f17dec31f1a1
Adam Kelly <adamkellyballintogher@gmail.com>
2025-10-07 22:14:46 +0100

    Added exmple pages and route's and components, for testing purpouses only

---
+++ anirank/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import ButtonCounter from "@/components/buttonCounter";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the About page.</p>
+      <ButtonCounter />
+    </div>
+  );
+}
---
+++ anirank/src/components/buttonCounter.tsx
@@ -0,0 +1,18 @@
+'use client'; // Needed for interactive components
+import { useState } from 'react';
+import { Button } from "@/components/ui/button"
+
+export default function buttonCounter() {
+  const [counter, setCounter] = useState(0); // Automaticlly updates the UI on state change
+
+  return (
+  <Button
+    variant="outline"
+    className='bg-neutral-950'
+    onClick={() => setCounter(counter + 1)}
+  >
+    You clicked the counter {counter} times
+  </Button>
+);
+
+}
