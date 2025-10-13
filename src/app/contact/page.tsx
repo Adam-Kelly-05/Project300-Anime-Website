ef9b95496da2d64cc6204c1885c6ff9ea7723ed2
Adam Kelly <adamkellyballintogher@gmail.com>
2025-10-13 22:08:05 +0100

    Moved example json data, and added carousel

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,11 @@
+import CarouselObject from "@/components/carousel";
+
 export default function AboutPage() {
   return (
     <div>
       <h1>Contact Us</h1>
       <p>This is the contact page.</p>
+      <CarouselObject />
     </div>
   );
 }
