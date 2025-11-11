50d9a65fbcb72c2a08a69a1d9bee69b41c9cbc6e
Adam Kelly <73916140+Adam-Kelly-05@users.noreply.github.com>
2025-11-11 10:28:11 +0000

    Updated next,js settings
    Added output and images configuration options.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
   experimental: {
     turbo: {
       root: __dirname,
     },
   },
+
   outputFileTracingRoot: __dirname,
 };
 
