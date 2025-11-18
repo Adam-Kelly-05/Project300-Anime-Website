83f435eb33f8bb937dc3a57bc1c700a970c6ef2f
PineappleGinge <daraorourke209@gmail.com>
2025-11-18 09:42:32 +0000

    added fetch but working on errors

--- src/components/reviews.tsx
+++ src/components/reviews.tsx
@@ -4,9 +4,20 @@ import reviews from "../extras/reviews.json"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
 
 export default function ReviewsObject() {
+    const [reviews, setReviews] = React.useState([]);
+
+    React.useEffect(() => {
+        async function fetchReviews() {
+            const response = await fetch("https://p7gfovbtqg.execute-api.eu-west-1.amazonaws.com/prod/reviews");
+            const reviews = await response.json();
+            setReviews(reviews);
+        }
+        fetchReviews();
+    }, []);
     return ( // returns all reviews
         <div className="space-y-4 p-4">
-            {reviews.map((review) => (
+            {
+            reviews.map((review) => (
                 <Card key={review.reviewId} className="bg-white text-black rounded-xl">
                     <CardHeader>
                         <CardTitle>{review.animeId}</CardTitle>
