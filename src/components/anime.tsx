fb6c16145519504d00d6183afb0a5a72b01bc488
PineappleGinge <daraorourke209@gmail.com>
2025-11-18 10:11:49 +0000

    made anime interface and restrucrured the anime.tsx file to get data from the dynamodb

--- src/components/anime.tsx
+++ src/components/anime.tsx
@@ -2,14 +2,14 @@
 
 import * as React from "react"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
-//import { Anime} from "@/types/anime"
+import { Anime} from "@/types/animes"
 
 export default function AnimesObject() {
     const [animes, setAnime] = React.useState<Anime[]>([]);
 
     React.useEffect(() => {
         async function fetchAnime() {
-            const response = await fetch("https://p7gfovbtqg.execute-api.eu-west-1.amazonaws.com/prod/reviews");
+            const response = await fetch("https://p7gfovbtqg.execute-api.eu-west-1.amazonaws.com/prod/anime");
             const reviews = await response.json();
             setAnime(animes);
         }
@@ -19,17 +19,21 @@ export default function AnimesObject() {
         <div className="space-y-4 p-4">
             {
             animes.map((anime) => (
-                <Card key={anime.animeId} className="bg-white text-black rounded-xl">
-                    <CardHeader>
-                        <CardTitle>{anime.animeName}</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <CardDescription>{anime.animeHeader}</CardDescription>
-                        <CardDescription>{anime.animeBody}</CardDescription>
-                        <CardDescription>Rating: {anime.rating}/10</CardDescription>
-                    </CardContent>
-                </Card>
-            ))}
+    <Card key={anime.animeId} className="bg-white text-black rounded-xl">
+        <CardHeader>
+            <CardTitle>{anime.title_english}</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+            <CardDescription><strong>Studio:</strong> {anime.studio}</CardDescription>
+            <CardDescription><strong>Aired:</strong> {anime.aired}</CardDescription>
+            <CardDescription><strong>Episodes:</strong> {anime.episodes}</CardDescription>
+            <CardDescription><strong>Type:</strong> {anime.type}</CardDescription>
+            <CardDescription><strong>Source:</strong> {anime.source}</CardDescription>
+            <CardDescription><strong>Synopsis:</strong> {anime.synopsis}</CardDescription>
+        </CardContent>
+    </Card>
+))}
         </div>
     )
 }
---
+++ src/types/animes.ts
@@ -0,0 +1,14 @@
+export interface Anime {
+  animeId: number;
+  aired: string;
+  episodes: number;
+  genres: string[];
+  image: string;
+  source: string;
+  studio: string;
+  synopsis: string;
+  title_english: string;
+  title_japanese: string;
+  trailer: string;
+  type: string;
+}
