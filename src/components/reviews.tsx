6902044097125f00a17abdc065d13195ab93df54
PineappleGinge <daraorourke209@gmail.com>
2025-11-18 09:34:08 +0000

    getting review page ready for conencting to apigateway

--- src/components/reviews.tsx
+++ src/components/reviews.tsx
@@ -6,24 +6,17 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/
 export default function ReviewsObject() {
     return ( // returns all reviews
         <div className="space-y-4 p-4">
-            {anime.map((animeItem) => {
-                const animesReviews = reviews.filter((review) => review.animeId === animeItem.animeId)
-                return ( // returns each review
-                    <div key={animeItem.animeId} className="space-y-4">
-                        {animesReviews.map((review) => (
-                            <Card key={review.reviewId} className="bg-white text-black rounded-xl">
-                                <CardHeader>
-                                    <CardTitle>{animeItem.englishTitle}</CardTitle>
-                                </CardHeader>
-                                <CardContent>
-                                    <CardDescription>{review.reviewText}</CardDescription>
-                                    <CardDescription>Rating: {review.score}/10</CardDescription>
-                                </CardContent>
-                            </Card>
-                        ))}
-                    </div>
-                )
-            })}
+            {reviews.map((review) => (
+                <Card key={review.reviewId} className="bg-white text-black rounded-xl">
+                    <CardHeader>
+                        <CardTitle>{review.animeId}</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <CardDescription>{review.reviewText}</CardDescription>
+                        <CardDescription>Rating: {review.score}/10</CardDescription>
+                    </CardContent>
+                </Card>
+            ))}
         </div>
     )
 }
